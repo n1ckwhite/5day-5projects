46f558854d472400eb8b155c058549f89e901d2e
Nick White <bbycinka@gmail.com>
2022-10-13 21:42:22 +0300

    refact: универсальный выбор класса

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const slides = document.querySelectorAll('.slide')
+const slidesStart = (acitveSlide = 0) => {
+  const slides = document.querySelectorAll('.slide')
+  slides[acitveSlide].classList.add('active')
+  slides.forEach(slide => {
+      slide.addEventListener('click', () => {
+          cliearActiveClasses()
+          slide.classList.add('active')
+      })
+  })
 
-slides.forEach(slide => {
-    slide.addEventListener('click', () => {
-        cliearActiveClasses()
-        slide.classList.add('active')
-    })
-})
+  const cliearActiveClasses = () => slides.forEach(i => i.classList.remove('active'))
+}
 
-const cliearActiveClasses = () => slides.forEach(i => i.classList.remove('active'))
+slidesStart(3)
